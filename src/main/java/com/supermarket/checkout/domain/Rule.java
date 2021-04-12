10ff4e3bbefe0cb8e94e64dfaebb3f67765cf8af
cpen5 <cpituse@gmail.com>
2021-04-12 23:02:25 +1000

    http test script

--- src/main/java/com/supermarket/checkout/domain/Rule.java
+++ src/main/java/com/supermarket/checkout/domain/Rule.java
@@ -1,28 +1,14 @@
 package com.supermarket.checkout.domain;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
-
+@Data
 public class Rule {
     private String itemName;
     private String rule;
 
-    public String getItemName() {
-        return itemName;
-    }
 
-
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-
-    public String getRule() {
-        return rule;
-    }
-
-    public void setRule(String rule) {
-        this.rule = rule;
-    }
 }
